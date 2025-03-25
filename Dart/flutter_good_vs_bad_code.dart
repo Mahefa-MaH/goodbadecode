70e796866fb5161afb2c216e12dbc659dbde13d7
Mahefa <102854581+Mahefa-MaH@users.noreply.github.com>
2025-03-25 14:51:15 +0300

    Created Dart/flutter_good_vs_bad_code.dart via auto push

---
+++ Dart/flutter_good_vs_bad_code.dart
@@ -0,0 +1,80 @@
+// Good Code Example:  Using a StatefulWidget for dynamic updates and managing state effectively.
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Good Code Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+// Bad Code Example:  Mixing UI logic and state management within the build method, leading to inefficiency and difficulty in maintenance.
+class MyBadWidget extends StatelessWidget {
+  int _counter = 0; //State directly in the widget, Bad practice
+
+  void _incrementCounter() {
+    _counter++; // Modifying state directly, will not trigger rebuild
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bad Code Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
