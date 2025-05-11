d4b5e0abe37db95ae5e982c91a1a95dfe3433460
Mahefa <102854581+Mahefa-MaH@users.noreply.github.com>
2025-05-11 09:30:58 +0300

    Updated Objective-C/objectivec_goodvsbadcode.m

--- Objective-C/objectivec_goodvsbadcode.m
+++ Objective-C/objectivec_goodvsbadcode.m
@@ -1,64 +1,61 @@
-// Good Code: Using Blocks for Asynchronous Operations
+// Good Code: Using blocks for asynchronous operations and error handling
 
-@interface MyViewController : UIViewController
+#import <Foundation/Foundation.h>
 
-@property (nonatomic, strong) NSURLSessionDataTask *dataTask;
+@interface MyManager : NSObject
+
+- (void)performLongRunningTaskWithCompletion:(void (^)(NSData *data, NSError *error))completion;
 
 @end
 
-@implementation MyViewController
+@implementation MyManager
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)performLongRunningTaskWithCompletion:(void (^)(NSData *data, NSError *error))completion {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // Simulate a long-running task
+        sleep(2); 
+        NSData *data = [@"This is some data" dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error = nil;
 
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURL *url = [NSURL URLWithString:@"https://www.example.com"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        if (/* some error condition */) {
+            error = [NSError errorWithDomain:@"MyDomain" code:1001 userInfo:@{NSLocalizedDescriptionKey: @"Something went wrong"}];
+        }
 
-    self.dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         dispatch_async(dispatch_get_main_queue(), ^{
+            completion(data, error);
+        });
+    });
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        MyManager *manager = [[MyManager alloc] init];
+        [manager performLongRunningTaskWithCompletion:^(NSData *data, NSError *error) {
             if (error) {
                 NSLog(@"Error: %@", error);
             } else {
-                NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                NSLog(@"Response: %@", responseString);
+                NSString *result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                NSLog(@"Result: %@", result);
             }
-        });
-    }];
-
-    [self.dataTask resume];
-}
-
-- (void)dealloc {
-    [self.dataTask cancel];
-}
-
-@end
-
-
-// Bad Code: Ignoring Error Handling and Using Deprecated Methods
-
-@interface MyViewControllerBad : UIViewController
-
-@end
-
-@implementation MyViewControllerBad
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    NSURL *url = [NSURL URLWithString:@"https://www.example.com"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-    [connection start];
+        }];
+        [[NSRunLoop currentRunLoop] run]; // Keep the main thread running
+    }
+    return 0;
 }
 
 
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"Response: %@", responseString); //No error handling
+
+// Bad Code:  Ignoring error handling and using deprecated methods
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *filePath = @"/path/to/file.txt"; // Replace with a valid path
+        NSString *fileContent = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil]; //Ignoring error
+        NSLog(@"File content: %@", fileContent);
+    }
+    return 0;
 }
-
-//Missing other NSURLConnection delegate methods for complete error handling
-
-@end
