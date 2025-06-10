88e98ac6b20ab81c3faea7ebdae3f124c255c79c
Mahefa <102854581+Mahefa-MaH@users.noreply.github.com>
2025-06-10 18:30:52 +0300

    Updated Fortran/fortran_good_bad_code.f90

--- Fortran/fortran_good_bad_code.f90
+++ Fortran/fortran_good_bad_code.f90
@@ -2,39 +2,39 @@ program good_code
   implicit none
   integer, allocatable :: arr(:)
   integer :: n, i, sum
-  
+  ! Get the size of the array from the user.
   print *, "Enter the size of the array:"
   read *, n
-  
+  ! Allocate memory for the array.
   allocate(arr(n))
-  
-  print *, "Enter the elements of the array:"
-  read *, (arr(i), i=1,n)
-  
+  ! Read the array elements from the user.
+  print *, "Enter the array elements:"
+  read *, (arr(i), i = 1, n)
+  ! Calculate the sum of the array elements.
   sum = 0
   do i = 1, n
     sum = sum + arr(i)
-  enddo
-  
-  print *, "Sum of array elements:", sum
+  end do
+  ! Print the sum of the array elements.
+  print *, "The sum of the array elements is:", sum
+  ! Deallocate the array memory.
   deallocate(arr)
 end program good_code
-
 
 program bad_code
   implicit none
   integer :: arr(100), n, i, sum
-  
-  print *, "Enter the size of the array (max 100):"
+  ! Get the size of the array from the user.  No error handling for exceeding bounds.
+  print *, "Enter the size of the array:"
   read *, n
-  
-  print *, "Enter the elements of the array:"
-  read *, (arr(i), i=1,n)
-  
+  ! Read the array elements from the user. No bounds checking.
+  print *, "Enter the array elements:"
+  read *, (arr(i), i = 1, n)
+  ! Calculate the sum of the array elements.
   sum = 0
-  do i = 1, 100 
+  do i = 1, n
     sum = sum + arr(i)
-  enddo
-  
-  print *, "Sum of array elements:", sum
+  end do
+  ! Print the sum of the array elements.
+  print *, "The sum of the array elements is:", sum
 end program bad_code
